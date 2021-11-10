91342db1a067ded90c7123bb4078c7822c0780de
Alexander Kozyrev <alex18_89@mail.ru>
2021-11-10 09:53:00 +0300

    Задача 5. Наименьший делитель

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def smallest_divisor_number(number):
+    smallest_divisor = 2
+    while (number / smallest_divisor) % 1 != 0:
+        smallest_divisor += 1
+    print('Наименьший делитель, отличный от единицы: ', smallest_divisor)
+
+
+
+number_N = int(input('Введите натуральное число N, больше единицы: '))
+while number_N <= 1:
+    print('Вы ввели неправильное число, попробуйте еще раз!')
+    number_N = int(input('Введите натуральное число N, больше единицы: '))
+
+smallest_divisor_number(number_N)
