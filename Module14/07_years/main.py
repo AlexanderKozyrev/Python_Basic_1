ce93291e0efb1c0fb4897800be1f9391cdf6c27c
Alexander Kozyrev <alex18_89@mail.ru>
2021-11-10 12:08:42 +0300

    Задача 7. Года

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def three_identical_numbers(number):
+    number_4 = number % 10
+    number_3 = (number % 100) // 10
+    number_2 = (number // 100) % 10
+    number_1 = number // 1000
+    if number_1 == number_2 == number_3 or number_1 == number_2 == number_4 or number_1 == number_3 == number_4 or number_2 == number_3 == number_4:
+        print(number)
+years_one = int(input('Введите первый год: '))
+years_two = int(input('Введите второй год: '))
+print('Года от ',years_one, 'до ', years_two, 'с тремя одинаковыми цифрами: ')
+while years_one != years_two:
+    three_identical_numbers(years_one)
+    years_one += 1
