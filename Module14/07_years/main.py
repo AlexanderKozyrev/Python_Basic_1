b9699d92b773c211274a1f2e9d1ea0e656d14997
Alexander Kozyrev <alex18_89@mail.ru>
2021-11-18 13:39:03 +0300

    Задача 7. Года. Исправления PEP8. Исправление длинной строки.

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -3,10 +3,12 @@ def three_identical_numbers(number):
     number_3 = (number % 100) // 10
     number_2 = (number // 100) % 10
     number_1 = number // 1000
-    # TODO вышли за границы нужно перенести строку, но знак \ не используем, лучше взять все в скобки
-    # TODO https://tirinox.ru/new-line/
-    if number_1 == number_2 == number_3 or number_1 == number_2 == number_4 or number_1 == number_3 == number_4 or number_2 == number_3 == number_4:
+
+    if (number_1 == number_2 == number_3 or number_1 == number_2 == number_4 or number_1 == number_3 == number_4
+        or number_2 == number_3 == number_4):
         print(number)
+
+
 years_one = int(input('Введите первый год: '))
 years_two = int(input('Введите второй год: '))
 print('Года от ', years_one, 'до ', years_two, 'с тремя одинаковыми цифрами: ')
@@ -14,4 +16,3 @@ while years_one != years_two:
     three_identical_numbers(years_one)
     years_one += 1
 
-# TODO есть недочеты по PEP
