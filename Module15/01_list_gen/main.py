fd5b59ce46636f48ef0f5362c6d3e5f6d6827c19
Alexander Kozyrev <alex18_89@mail.ru>
2021-11-30 19:52:36 +0300

    Задача 1

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,9 +1,8 @@
-# TODO здесь писать код
 number_N = int(input('Введите целое число: '))
 score_odd = []
 
 for number in range(number_N + 1):
-    if number // 2 == 0:
+    if number % 2 != 0:
         score_odd.append(number)
 
 print(score_odd)
