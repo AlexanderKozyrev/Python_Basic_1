07b28948da8c4fd9a8397f132fb73ea9b6b26f6b
Alexander Kozyrev <alex18_89@mail.ru>
2021-12-02 22:30:54 +0300

    Задача 6

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+word = input('Введите слово: ')
+word = list(word)
+unique_letters = 0
+
+for i_word in word:
+    coincidences = 0
+    for y_word in word:
+        if i_word == y_word:
+            coincidences += 1
+    if coincidences == 1:
+        unique_letters += 1
+
+print('Количество уникальных букв: ', unique_letters)
