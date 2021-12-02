343bb59cb69b06a96012aaa2cdd756d397e7158a
Alexander Kozyrev <alex18_89@mail.ru>
2021-12-02 21:55:08 +0300

    Задача 5

--- Module15/05_movie/main.py
+++ Module15/05_movie/main.py
@@ -1,5 +1,15 @@
 films = ['Крепкий орешек', 'Назад в будущее', 'Таксист',
          'Леон', 'Богемская рапсодия', 'Город грехов',
          'Мементо', 'Отступники', 'Деревня']
+favourite_films = []
+favourite_film = input('Введите фильм: ')
+while favourite_film != 'end':
+    for film in films:
+        if film == favourite_film:
+            favourite_films.append(film)
+    print('Ошибка, такого фильма нет.')
+    favourite_film = input('Введите фильм: ')
 
-# TODO здесь писать код
+print('Список любимых фильмов: ', favorite_films)
+
+
