6ef8cb85671c047774d93147ce6e536440f19d77
Alexander Kozyrev <alex18_89@mail.ru>
2021-12-02 23:00:43 +0300

    Задача 7

--- Module15/07_containers/main.py
+++ Module15/07_containers/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+number_containers = int(input('Введите количество контейнеров: '))
+weight_containers = []
+
+for number in range(number_containers):
+    weight_container = int(input('Введите вес контейнера: '))
+    while weight_container > 200:
+        print(' Вес контейнера не должен превышать 200кг, введите вес еще раз ', end='')
+        weight_container = int(input())
+    weight_containers.append(weight_container)
+
+new_weight_container = int(input('Введите вес нового контейнера: '))
+while new_weight_container > 200:
+    print(' Вес контейнера не должен превышать 200кг, введите вес еще раз ', end='')
+    new_weight_container = int(input())
+
+for new_number in range(number_containers):
+    if weight_containers[new_number] < new_weight_container:
+        print('Номер, куда встанет новый контейнер: ', new_number + 1)
+        break
+
