358b588f78dcb53b5d5b4f3b99221ec6c9e92cd0
Alexander Kozyrev <alex18_89@mail.ru>
2021-12-02 20:35:03 +0300

    Задача 3

--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+
+list_names = ['Артемий', 'Борис', 'Влад', 'Гоша', 'Дима', 'Евгений', 'Женя', 'Захар']
+new_list_names = []
+
+for number in 1, 3, 5, 7:
+    new_list_names.append(list_names(number))
+
+print(new_list_names)
--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+number_cells = int(input('Введите количество клеток: '))
+unsuitable_values = []
+
+for number in range(number_cells):
+    print('Эффективность', number + 1, 'клетки: ')
+    effectiveness = int(input(''))
+    if effectiveness < number:
+        unsuitable_values.append(effectiveness)
+
+print('Неподходящие значения', unsuitable_values)
