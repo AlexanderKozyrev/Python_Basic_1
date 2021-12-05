af5cccf3d9533959c675e758018bec856d2e80a2
Alexander Kozyrev <alex18_89@mail.ru>
2021-12-05 19:53:38 +0300

    Задача 9

--- Module15/09_word_analysis_2/main.py
+++ Module15/09_word_analysis_2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+word = input('Введите слово: ')
+word = list(word)
+opposite_word = []
+index = 1
+
+for _ in range(len(word)):
+    opposite_word.append(word[-index])
+    index += 1
+
+if word == opposite_word:
+    print('Слово является палиндромом.')
+else:
+    print('Слово не является палиндромом')
+
