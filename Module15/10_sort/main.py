e9ad1aac844e64dc8bc441fbae71b5c140baf809
Alexander Kozyrev <alex18_89@mail.ru>
2021-12-05 20:45:26 +0300

    Задача 10

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+number_N = int(input("Кол-во элементов: "))
+start_list = []
+
+for _ in range(number_N):
+    element = input("Введите элемент списка:")
+    start_list.append(element)
+
+for i in range(number_N - 1):
+    minimum = i
+
+    for j in range(i + 1, number_N):
+        if start_list[j] < start_list[minimum]:
+            minimum = j
+    start_list[minimum], start_list[i] = start_list[i], start_list[minimum]
+
+print('Отсортированный список: ', start_list)
+
+
