862f69207bc4cd016ec9522b27a15630ffabd19f
Alexander Kozyrev <alex18_89@mail.ru>
2021-12-05 19:39:17 +0300

    Задача 8

--- Module15/08_running_nums/main.py
+++ Module15/08_running_nums/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+number_N = int(input("Кол-во элементов: "))
+start_list = []
+
+for _ in range(number_N):
+    element = input("Введите элемент списка:")
+    start_list.append(element)
+
+shift = int(input("Сдвиг: "))
+index = 0
+while index < number_N - 1:
+    empty_place = start_list[index]
+    start_list[index] = start_list[index + shift]
+    start_list[index + shift] = empty_place
+    index += 1
+
+print('Сдвинутый список: ', start_list)
