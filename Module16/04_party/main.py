829218e1af00723a12f01a704a05ed4b3f07f070
Alexander Kozyrev <alex18_89@mail.ru>
2021-12-08 16:39:31 +0300

    Задача 4

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,3 +1,23 @@
 guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
 
-# TODO здесь писать код
+while True:
+    print('Сейчас на вечеринке', len(guests), 'человек:', guests)
+    action = input('Гость пришел или ушел? ')
+    if action == 'пришел':
+        name = input('Введите имя гостя: ')
+        print('Привет, ', name)
+        if len(guests) == 6:
+            print('Прости ', name, 'но мест нет')
+        else:
+            guests.append(name)
+
+    if action == 'ушел':
+        name = input('Введите имя гостя: ')
+        print('Пока, ', name)
+        guests.remove(name)
+
+    if action == 'Пора спать':
+        print('Вечеринка закончилась, пора спать.')
+        break
+    print()
+
