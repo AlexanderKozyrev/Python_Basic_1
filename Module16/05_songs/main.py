fc2f9c73340ed9c0ed67f955463a753e8f1b7f31
Alexander Kozyrev <alex18_89@mail.ru>
2021-12-08 20:35:54 +0300

    Задача 5

--- Module16/05_songs/main.py
+++ Module16/05_songs/main.py
@@ -9,5 +9,17 @@ violator_songs = [
     ['Blue Dress', 4.29],
     ['Clean', 5.83]
 ]
+number_songs = int(input('Сколько песен выбрать:  '))
+songs_time = 0
 
-# TODO здесь писать код
+for song in range(number_songs):
+    name_song = input(f'Название {song + 1} песни: ')
+    for track in violator_songs:
+        for i in track:
+            if name_song == i:
+                songs_time += track[1]
+
+print('Общее время звучания песен: ', round(songs_time, 2), 'минут')
+
+
+
