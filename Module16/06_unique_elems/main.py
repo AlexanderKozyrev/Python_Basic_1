b5e9bc039eab9c04227238ad228f1859444d72c9
Alexander Kozyrev <alex18_89@mail.ru>
2021-12-08 20:54:39 +0300

    Задача 6

--- Module16/06_unique_elems/main.py
+++ Module16/06_unique_elems/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+numbers_one = []
+numbers_two = []
+
+for _ in range(3):
+    number_one = int(input('Введите число в первый список: '))
+    numbers_one.append(number_one)
+
+for _ in range(7):
+    number_two = int(input('Введите число во второй список: '))
+    numbers_two.append(number_two)
+
+numbers_one.extend(numbers_two)
+
+numbers_one = list(set(numbers_one))
+
+print(numbers_one)
+
