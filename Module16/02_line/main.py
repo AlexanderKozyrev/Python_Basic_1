7979137f06e9ad496a0fad078bb60c843874d6a6
Alexander Kozyrev <alex18_89@mail.ru>
2021-12-08 15:17:14 +0300

    Задача 2

--- Module16/02_line/main.py
+++ Module16/02_line/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+rank_one = []
+rank_two = []
+
+for height in range(160, 178, 2):
+    rank_one.append(height)
+
+for height in range(162, 183, 3):
+    rank_two.append(height)
+
+rank_one.extend(rank_two)
+
+for i in range(len(rank_one)):
+    for j in range(i + 1, len(rank_one)):
+        if rank_one[j] < rank_one[i]:
+            rank_one[i], rank_one[j] = rank_one[j], rank_one[i]
+
+print('Отсортированный список: ', rank_one)
+
