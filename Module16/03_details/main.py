3972892fffa88b6a3d74cf67fe244b5bf73de5e8
Alexander Kozyrev <alex18_89@mail.ru>
2021-12-08 15:44:29 +0300

    Задача 3

--- Module16/03_details/main.py
+++ Module16/03_details/main.py
@@ -2,4 +2,15 @@ shop = [['каретка', 1200], ['шатун', 1000], ['седло', 300],
         ['педаль', 100], ['седло', 1500], ['рама', 12000],
         ['обод', 2000], ['шатун', 200], ['седло', 2700]]
 
-# TODO здесь писать код
+detail = input('Введите название детали: ')
+quantity_detail = 0
+total_cost = 0
+
+for i in shop:
+        quantity_detail += i.count(detail)
+        if i.count(detail) > 0:
+                total_cost += i[1]
+
+print('Количество деталей: ', quantity_detail)
+print('Общая стоимость: ', total_cost)
+
