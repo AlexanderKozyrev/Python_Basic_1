97c0b6ea0df163490b6ab05f9bdeae9bf6c5e9b6
Alexander Kozyrev <alex18_89@mail.ru>
2021-12-09 13:21:38 +0300

    Задача 9

--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+friends = int(input('Введите количество друзей: '))
+receipts = int(input('Введите количество олговых расписок: '))
+registry_friends = []
+
+for i in range(friends):
+    registry_friends.append(0)
+
+for receipt in range(receipts):
+    print(receipt + 1, 'расписка')
+    to_whom = int(input('Кому: '))
+    from_whom = int(input('От кого: '))
+    how_many = int(input('Сколько: '))
+    registry_friends[to_whom - 1] += how_many
+    registry_friends[from_whom - 1] -= how_many
+
+print('Балнс друзей: ')
+for index in range(friends):
+    print(index + 1, ':', registry_friends[index])
