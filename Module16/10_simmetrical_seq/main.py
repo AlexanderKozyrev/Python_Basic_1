86a771753dc1dfacf76db5aa18cd8d390da4f55d
Alexander Kozyrev <alex18_89@mail.ru>
2021-12-09 17:06:02 +0300

    Задача 10

--- Module16/10_simmetrical_seq/main.py
+++ Module16/10_simmetrical_seq/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+numbers = int(input('Введите количество чисел: '))
+num = []
+
+for index in range(numbers):
+    number = int(input('Число: '))
+    num.append(number)
+print('Последовательность: ', end=' ')
+for index in range(numbers):
+    print(num[index], end=' ')
