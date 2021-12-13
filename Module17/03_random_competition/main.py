77d6400d13ca167ed21e7282f573cec17b2f1336
Alexander Kozyrev <alex18_89@mail.ru>
2021-12-13 21:50:48 +0300

    Задача 3

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+import random
+
+first_team = [round((random.uniform(5, 10)), 2) for _ in range(20)]
+second_team = [round((random.uniform(5, 10)), 2) for _ in range(20)]
+winners = []
+
+for i in range(20):
+    if first_team[i] > second_team[i]:
+        winners.append(first_team[i])
+    else:
+        winners.append(second_team[i])
+
+print('Первая команда: ', first_team)
+print('Вторая команда: ', second_team)
+print('Победителя тура: ', winners)
