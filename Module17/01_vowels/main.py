2f5f9395d062e0ad1e12cf2a0efb35bb247f6acd
Alexander Kozyrev <alex18_89@mail.ru>
2021-12-13 21:16:03 +0300

    Задача 1

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+text = input('Введите текст: ')
+vowel_letters = ['а', 'о', 'э', 'е', 'и', 'ы', 'у', 'ё', 'ю', 'я']
+vowel_letters_text = []
+
+for i in range(len(text)):
+    for j in range(len(vowel_letters)):
+        if vowel_letters[j] == text[i]:
+            vowel_letters_text.append(text[i])
+
+print('Список гласных букв: ', vowel_letters_text)
+print('Длина списка: ', len(vowel_letters_text))
+
