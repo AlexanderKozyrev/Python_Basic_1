5b50ce2e4924a8de4d9f1422c2c50b0a7383006e
Alexander Kozyrev <alex18_89@mail.ru>
2021-12-14 21:38:27 +0300

    Задача 6

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+import random
+
+number_N = int(input('Кол-во чисел в списке: '))
+before_compression = [random.randint(0, 2) for _ in range(number_N)]
+after_compression = []
+
+for i in range(number_N):
+    if before_compression[i] != 0:
+        after_compression.append(before_compression[i])
+
+print('Список до сжатия: ', before_compression)
+print('Список после сжатия: ', after_compression)
