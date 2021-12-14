71b8154b8c6d216f469266c8f4c5a97c717830c7
Alexander Kozyrev <alex18_89@mail.ru>
2021-12-14 22:29:51 +0300

    Задача 8

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+stick = int(input('Количество палок: '))
+sticks = ['I' for _ in range(stick)]
+
+throws = int(input('Количество бросков: '))
+for i in range(1, throws + 1):
+    throw_from = int(input(f'Бросок {i}, Сбиты палки с номера '))
+    throws_to = int(input('по номер '))
+    for j in range(throw_from - 1, throws_to):
+        sticks[j] = '.'
+
+print('Результат: ', end='')
+for i in range(stick):
+    print(sticks[i], end='')
+
