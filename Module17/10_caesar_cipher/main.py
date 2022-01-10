5489ae8203a050ff248b8bce2c0656e0d2d69643
Alexander Kozyrev <alex18_89@mail.ru>
2022-01-10 15:31:01 +0300

    Задача 10

--- Module17/10_caesar_cipher/main.py
+++ Module17/10_caesar_cipher/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+alpha = []
+for i in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя':
+    alpha.append(i)
+
+text = input('Введите сообщение: ')
+shift = int(input('Введите сдвиг: '))
+s = ''
+for i in text:
+    if i == ' ':
+        s = s + ' '
+    else:
+        k = alpha.index(i) + shift
+        if k > 32:
+            k = k - 33
+        s = s + alpha[k]
+
+print('Зашифрованное сообщение: ', s)
