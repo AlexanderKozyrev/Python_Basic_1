59d38f77d4d6e929118d6219a05766038427d2e6
Alexander Kozyrev <alex18_89@mail.ru>
2022-01-14 15:28:29 +0300

    Задача 5

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+password = input('Придумайте пароль: ')
+
+if len(password) < 8:
+    print('Пароль ненадёжный. Попробуйте ещё раз.')
+elif sum(map(str.isupper, password)) == 0:
+    print('Пароль ненадёжный. Попробуйте ещё раз.')
+elif sum(map(str.isdigit, password)) < 3:
+    print('Пароль ненадёжный. Попробуйте ещё раз.')
+else:
+    print('Это надёжный пароль!')
