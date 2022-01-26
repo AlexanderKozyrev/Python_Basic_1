9d971643f0cfe854c2d04f9ade9774f4bd7d25bc
Alexander Kozyrev <alex18_89@mail.ru>
2022-01-26 22:54:09 +0300

    Task_9

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+numbers = int(input('Введите количество человек:'))
+name_dict = {}
+height_dict = {}
+
+for number in range(numbers - 1):
+    name_descendant, name_parent = input(f'{number + 1} пара: ').split()
+    name_dict[name_descendant] = name_parent
+    height_dict[name_descendant] = 0
+    height_dict[name_parent] = 0
+print(height_dict)
+print(name_dict)
+for i_name in name_dict:
+    s_name = i_name
+    while s_name in name_dict:
+        s_name = name_dict[s_name]
+        height_dict[i_name] += 1
+        print(height_dict)
+
+for i_dict in sorted(height_dict):
+    print(i_dict, height_dict[i_dict])
+
