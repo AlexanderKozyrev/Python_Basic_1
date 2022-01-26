95a929fc4ef571f21c065e65024847927775846a
Alexander Kozyrev <alex18_89@mail.ru>
2022-01-26 21:50:16 +0300

    Task_7

--- Module19/07_pizza/main.py
+++ Module19/07_pizza/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+numbers = int(input('Введите количество заказов: '))
+name_dict = {}
+for number in range(1, numbers + 1):
+    order = input(f'{number} заказ: ').split()
+    name_dict[number] = order
+
+print(name_dict)
