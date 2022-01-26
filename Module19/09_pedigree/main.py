8013b8929d4f4b6d7296736bf4a3fedac1344518
Alexander Kozyrev <alex18_89@mail.ru>
2022-01-26 22:56:15 +0300

    Task_9

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -7,14 +7,12 @@ for number in range(numbers - 1):
     name_dict[name_descendant] = name_parent
     height_dict[name_descendant] = 0
     height_dict[name_parent] = 0
-print(height_dict)
-print(name_dict)
+
 for i_name in name_dict:
     s_name = i_name
     while s_name in name_dict:
         s_name = name_dict[s_name]
         height_dict[i_name] += 1
-        print(height_dict)
 
 for i_dict in sorted(height_dict):
     print(i_dict, height_dict[i_dict])
