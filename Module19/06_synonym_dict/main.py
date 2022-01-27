45f4e903622ad04f92286a26f1e1dd164eea310f
Alexander Kozyrev <alex18_89@mail.ru>
2022-01-27 19:15:34 +0300

    Task_6 Исправлены переменные

--- Module19/06_synonym_dict/main.py
+++ Module19/06_synonym_dict/main.py
@@ -1,19 +1,18 @@
 numbers_words = int(input('Введите количество пар слов: '))
-words_dict = {}
+words_dictionary = {}
 
 for number in range(numbers_words):
     couple = input(f'{number + 1} пара: ').lower().split(' - ')
-    words_dict[couple[0]] = couple[1]
+    words_dictionary[couple[0]] = couple[1]
 
-print(words_dict)
+print(words_dictionary)
 while True:
     word = input('Введите слово: ').lower()
-    if word in words_dict:
-        print('Синоним: ', words_dict[word])
-    elif word in words_dict.values():
-        print('Синоним: ', words_dict.values(word))
+    if word in words_dictionary:
+        print('Синоним: ', words_dictionary[word])
+    elif word in words_dictionary.values():
+        print('Синоним: ', words_dictionary.values(word))
     else:
         print('Такого слова в словаре нет.')
 
-# TODO применить рекомендации данные ранее
 
