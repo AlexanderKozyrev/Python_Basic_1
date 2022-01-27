468db725c069dc15d81ddeccf489c196999bba39
Alexander Kozyrev <alex18_89@mail.ru>
2022-01-27 19:08:05 +0300

    Task_2 Исправлены переменные

--- Module19/02_geography/main.py
+++ Module19/02_geography/main.py
@@ -1,16 +1,14 @@
 numbers_countries = int(input('Количество стран: '))
-# TODO в именовании переменных не используем list set dict tuple
-countries_dict = {}
+countries = {}
 
-# TODO не рекомендую использовать прификс i_
-for i_number in range(0, numbers_countries):
-    country = input(f'{i_number + 1} страна: ').split()
-    countries_dict[country[0]] = country[1:]
+for number in range(0, numbers_countries):
+    country = input(f'{number + 1} страна: ').split()
+    countries[country[0]] = country[1:]
 
-for number in range(1, 4):
-    town = input(f'{number} город: ')
-    for country in countries_dict:
-        if town in countries_dict[country]:
+for num in range(1, 4):
+    town = input(f'{num} город: ')
+    for country in countries:
+        if town in countries[country]:
             print(f'Город {town} расположен в стране {country}')
         else:
             print(f'По городу {town} данных нет')
