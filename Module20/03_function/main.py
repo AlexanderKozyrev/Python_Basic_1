ee0376fda311567679b9629020f6625e8fbfa9ea
Alexander Kozyrev <alex18_89@mail.ru>
2022-01-31 22:32:49 +0300

    Task_3

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def slicer(text, number):
+    if text.count(number) == 0:
+        return ()
+    elif text.count(number) == 1:
+        new_text = text[text.index(number):]
+        return new_text
+    else:
+        new_text = text[(text.index(number)):(text.index(number, (text.index(number) + 1)) + 1)]
+        return new_text
+
+
+print(slicer((1, 2, 3, 4, 5, 6, 7, 8, 2, 2, 9, 10), 2))
