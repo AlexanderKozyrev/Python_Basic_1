aa0788909fcb806a708de2877bcc6521bd3476d7
Alexander Kozyrev <alex18_89@mail.ru>
2022-01-31 08:42:04 +0300

    Task_9 Исправлены переменные

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,22 +1,20 @@
 numbers = int(input('Введите количество человек:'))
-name_dict = {}
-height_dict = {}
+name_dictionary = {}
+height_dictionary = {}
 
 for number in range(numbers - 1):
     name_descendant, name_parent = input(f'{number + 1} пара: ').split()
-    name_dict[name_descendant] = name_parent
-    height_dict[name_descendant] = 0
-    height_dict[name_parent] = 0
+    name_dictionary[name_descendant] = name_parent
+    height_dictionary[name_descendant] = 0
+    height_dictionary[name_parent] = 0
 
-for i_name in name_dict:
-    s_name = i_name
-    while s_name in name_dict:
-        s_name = name_dict[s_name]
-        height_dict[i_name] += 1
+for name in name_dictionary:
+    s_name = name
+    while s_name in name_dictionary:
+        s_name = name_dictionary[s_name]
+        height_dictionary[name] += 1
 
-for i_dict in sorted(height_dict):
-    print(i_dict, height_dict[i_dict])
+for dictionary in sorted(height_dictionary):
+    print(dictionaryt, height_dictionary[dictionary])
 
-
-# TODO применить рекомендации данные ранее
 
