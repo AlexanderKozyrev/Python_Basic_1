abbf702e23ce9877d38498a89671a13f7b42f67e
Alexander Kozyrev <alex18_89@mail.ru>
2022-02-01 23:08:16 +0300

    Task_9

--- Module20/09_competition_protocol/main.py
+++ Module20/09_competition_protocol/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+recordings = int(input('Сколько записей вносится в протокол? '))
+protocol = {}
+print('Записи (результат и имя): ')
+for record in range(1, recordings + 1):
+
