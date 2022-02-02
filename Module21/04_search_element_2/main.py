d48cc4920910b7e2d1b59afbbc08ace797357c44
Alexander Kozyrev <alex18_89@mail.ru>
2022-02-02 22:00:29 +0300

    Task_4

--- Module21/04_search_element_2/main.py
+++ Module21/04_search_element_2/main.py
@@ -1,1 +1,30 @@
-# TODO здесь писать код
+site = {
+	'html': {
+		'head': {
+			'title': 'Мой сайт'
+		},
+		'body': {
+			'h2': 'Здесь будет мой заголовок',
+			'div': 'Тут, наверное, какой-то блок',
+			'p': 'А вот здесь новый абзац'
+		}
+	}
+}
+
+def find_key(struct, key):
+	if key in struct:
+		return struct[key]
+
+	for sub_struct in struct.values():
+		if isinstance(sub_struct, dict):
+			result = find_key(sub_struct, key)
+			if result:
+				break
+	else:
+		result = None
+		return result
+
+
+user_key = input('Введите искомый ключ: ')
+value = find_key(site, user_key)
+print('Значение ключа: ', value)
