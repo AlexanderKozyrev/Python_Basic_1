4476f54f21fb4fbf075caed7adf8bfa199a4aa9d
Alexander Kozyrev <alex18_89@mail.ru>
2022-02-02 21:01:40 +0300

    Task_2

--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def pairs(text, numbers):
+    my_pairs = ((text[elem], numbers[elem]) for elem in range(min(len(text), len(numbers))))
+    return my_pairs
+
+
+my_text = 'abdd'
+my_numbers = (10, 20, 30, 40)
+
+for element in pairs(my_text, my_numbers):
+    print(element)
