a49a893db3cd5c483a1c61b023cc4adcf793edee
Alexander Kozyrev <alex18_89@mail.ru>
2022-02-03 23:02:21 +0300

    Task_4

--- Module21/04_search_element_2/main.py
+++ Module21/04_search_element_2/main.py
@@ -12,13 +12,18 @@ site = {
 }
 
 
-def find_key(struct, key):
+def find_key(struct, key, max_depth=None, depth=1):
+	result = None
+
+	if max_depth and max_depth < depth:
+		return result
+
 	if key in struct:
 		return struct[key]
 
 	for sub_struct in struct.values():
 		if isinstance(sub_struct, dict):
-			result = find_key(sub_struct, key)
+			result = find_key(sub_struct, key, max_depth, depth=depth + 1)
 			if result:
 				break
 	else:
@@ -27,5 +32,12 @@ def find_key(struct, key):
 
 
 user_key = input('Введите искомый ключ: ')
-value = find_key(site, user_key)
-print('Значение ключа: ', value)
+deep = input('Хотите ввести максимальную глубину? Y/N: ')
+
+if deep == 'n':
+	value = find_key(site, user_key)
+	print('Значение ключа: ', value)
+else:
+	deep_max = int(input('Введите максимальную глубину:'))
+	value = find_key(site, user_key, max_depth=deep_max)
+	print('Значение ключа: ', value)
