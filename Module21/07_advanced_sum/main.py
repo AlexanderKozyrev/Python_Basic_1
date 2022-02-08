ebc52ae77bb70c2b156c7f65511a9c56d404b52c
Alexander Kozyrev <alex18_89@mail.ru>
2022-02-08 17:42:32 +0300

    Task_7

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,1 +1,15 @@
-print(sum([[1, 2, [3]], [1], 3]))
+def flatten(a_list):
+    result = 0
+    for number in a_list:
+        if isinstance(number, int):
+            result += number
+        else:
+            flatten(number)
+    return result
+
+
+amount = flatten([[1, 2, [3]], [1], 3])
+print(amount)
+
+amount = flatten((1, 2, 3, 4, 5))
+print(amount)
