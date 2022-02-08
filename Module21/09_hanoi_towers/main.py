70efeb129e103c165c93b1313c4b6e1d35c80194
Alexander Kozyrev <alex18_89@mail.ru>
2022-02-08 18:47:18 +0300

    Task_9

--- Module21/09_hanoi_towers/main.py
+++ Module21/09_hanoi_towers/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def move(n, start=1, finish=3):
+    if n == 1:
+        print("Перенести диск 1 со стержня", start, "на стержень", finish)
+    else:
+        temp = (6 - start) - finish
+        move(n - 1, start, temp)
+        print("Перенести диск", n, "со стержня", start, "на стержень", finish)
+        res = (n - 1, temp, finish)
+        print("Перенести диск", n-1, "со стержня", temp, "на стержень", finish)
+        return res
+
+
+numbers = int(input('Введите количество дисков: '))
+move(numbers)
