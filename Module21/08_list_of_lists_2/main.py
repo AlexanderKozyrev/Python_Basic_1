37660329bd739f736214ca3f475c47aaa147cf26
Alexander Kozyrev <alex18_89@mail.ru>
2022-02-08 17:51:29 +0300

    Task_8

--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,4 +1,16 @@
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+
+def expands_lists(a_list):
+    result = []
+    for number in a_list:
+        if isinstance(number, int):
+            result.append(number)
+        else:
+            result.extend(expands_lists(number))
+    return result
+
+
+new_nice_list = expands_lists(nice_list)
+print(new_nice_list)
