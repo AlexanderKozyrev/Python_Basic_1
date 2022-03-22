62af773be88ee9857fa02405cf3bab58079032f7
Alexander Kozyrev <alex18_89@mail.ru>
2022-03-22 13:07:24 +0300

    Task_4

--- Module22/04_files_and_folders/main.py
+++ Module22/04_files_and_folders/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+import os
+
+path = input('Пожалуйста, введите путь до директории:')
+total_size = 0
+path, dirs, files = next(os.walk(path))
+for name in files:
+    fp = os.path.join(path, name)
+    total_size += os.path.getsize(fp)
+
+print('Размер каталога (в Кб): ', total_size / 1024)
+print('Количество подкаталогов: ', len(dirs))
+print('Количество файлов: ', len(files))
