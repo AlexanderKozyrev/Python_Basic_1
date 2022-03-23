cb5fdd0987a2fbc24fe3c94786309530fa79bee4
Alexander Kozyrev <alex18_89@mail.ru>
2022-03-23 11:39:47 +0300

    Task 8

--- Module22/08_frequency_analysis/main.py
+++ Module22/08_frequency_analysis/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+import re
+from collections import Counter
+
+file = open('text.txt', 'r')
+text = ''
+for line in file:
+    text = text + line
+
+letters = Counter(re.findall('[a-z]', text.lower()))
+#Как отсортировать словарь по значениям и по ключам?
+for letter in letters:
+    print(f'{letter} {letters[letter] / sum(letters.values()):.3f}')
