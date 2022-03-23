1468b5c94a6530ac76b74ef72be2b637de4a9747
Alexander Kozyrev <alex18_89@mail.ru>
2022-03-23 15:16:01 +0300

    Task 8

--- Module22/08_frequency_analysis/main.py
+++ Module22/08_frequency_analysis/main.py
@@ -9,4 +9,8 @@ for line in file:
 letters = Counter(re.findall('[a-z]', text.lower()))
 #Как отсортировать словарь по значениям и по ключам?
 for letter in letters:
-    print(f'{letter} {letters[letter] / sum(letters.values()):.3f}')
+    with open('analysis.txt', 'a') as file_new:
+        file_new.write(letter + ' ')
+        number = round((letters[letter] / sum(letters.values())), 3)
+        number = str(number)
+        file_new.write(number + '\n')
