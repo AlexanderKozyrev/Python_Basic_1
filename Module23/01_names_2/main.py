1e413deb9cd22735618d229b8a17dafd17b0d2cc
Alexander Kozyrev <alex18_89@mail.ru>
2022-03-24 15:18:54 +0300

    Task 1

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -5,11 +5,14 @@ file = open('people.txt', 'r', encoding='utf-8')
 for word in file:
     line_count += 1
     lenght = len(word)
-    if word.endswith("\n"):
-        lenght -= 1
-    if lenght < 3:
-        raise BaseException('Длина {} строки меньше 3 символов'.format(line_count))
-    numbers_sumbol += lenght
+    try:
+        if word.endswith("\n"):
+            lenght -= 1
+        if lenght < 3:
+            raise BaseException
+        numbers_sumbol += lenght
+    except BaseException:
+        print('Длина {} строки меньше 3 символов'.format(line_count))
 
 file.close()
 print('Найденная сумма символовю ', numbers_sumbol)
