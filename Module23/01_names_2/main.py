2c8e19456508a1f577ae6f2a23b3cc43d65c1f9e
Alexander Kozyrev <alex18_89@mail.ru>
2022-03-24 08:44:07 +0300

    Task 1

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+numbers_sumbol = 0
+line_count = 0
+
+file = open('people.txt', 'r', encoding='utf-8')
+for word in file:
+    line_count += 1
+    lenght = len(word)
+    if word.endswith("\n"):
+        lenght -= 1
+    if lenght < 3:
+        raise BaseException('Длина {} строки меньше 3 символов'.format(line_count))
+
+    numbers_sumbol += lenght
+
+file.close()
+finally:
+    print('Найденная сумма символовю ', numbers_sumbol)
