0530be773631a50aa075a0a8fc557546ddb29e1b
Alexander Kozyrev <alex18_89@mail.ru>
2022-03-25 16:41:32 +0300

    Task 4

--- Module23/04_registration/main.py
+++ Module23/04_registration/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+def exceptions(text):
+    try:
+        text = text.split()
+        if len(text) != 3:
+            raise IndexError
+        if text[0].isalpha() = False:
+            raise NameError
+
+
+
+    except IndexError:
+        print('НЕ присутствуют все три поля')
+    except NameError:
+
+
+with open('registrations.txt', 'r', encoding='utf-8') as file:
+    for line in file:
+        print(len(line))
+
