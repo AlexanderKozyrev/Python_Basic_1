18994766c2a3b76e32786203a454d870e0036618
Alexander Kozyrev <alex18_89@mail.ru>
2022-03-28 10:14:22 +0300

    Task 4

--- Module23/04_registration/main.py
+++ Module23/04_registration/main.py
@@ -3,17 +3,34 @@ def exceptions(text):
         text = text.split()
         if len(text) != 3:
             raise IndexError
-        if text[0].isalpha() = False:
+        if not text[0].isalpha():
             raise NameError
-
-
+        if '@' and '.' not in text[1]:
+            raise SyntaxError
+        if not 0 < int(text[2]) < 99:
+            raise ValueError
+        else:
+            with open('registraton_good.log', mode='a', encoding='utf-8') as good:
+                good.write(line + '\n')
 
     except IndexError:
-        print('НЕ присутствуют все три поля')
+        with open ('registration_bad.log', mode='a', encoding='utf-8') as bad:
+            bad.write(line + '     НЕ присутствуют все три поля' + '\n')
+
     except NameError:
+        with open ('registration_bad.log', mode='a', encoding='utf-8') as bad:
+            bad.write(line + '     поле имени содержит НЕ только буквы:' + '\n')
+
+    except SyntaxError:
+        with open ('registration_bad.log', mode='a', encoding='utf-8') as bad:
+            bad.write(line + '     поле емейл НЕ содержит @ и .(точку):' + '\n')
+
+    except ValueError:
+        with open ('registration_bad.log', mode='a', encoding='utf-8') as bad:
+            bad.write(line + '     поле возраст НЕ является числом от 10 до 99:' + '\n')
 
 
 with open('registrations.txt', 'r', encoding='utf-8') as file:
     for line in file:
-        print(len(line))
+        exceptions(line)
 
