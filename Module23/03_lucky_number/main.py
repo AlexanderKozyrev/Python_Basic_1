0ed49a8b16407d2149b5b4e9908229bc3840b35c
Alexander Kozyrev <alex18_89@mail.ru>
2022-03-29 18:45:39 +0300

    Task 3

--- Module23/03_lucky_number/main.py
+++ Module23/03_lucky_number/main.py
@@ -1,5 +1,10 @@
 import random
 
+
+class MyFirstException(Exception):
+    pass
+
+
 numbers_sum = 0
 try:
     while numbers_sum < 777:
@@ -7,12 +12,12 @@ try:
         numbers_sum += number
         probability = random.randint(1, 13)
         if probability == 1:
-            raise BaseException
+            raise MyFirstException
 
         with open('out_file.txt', 'a') as file:
             file.write(str(number) + '\n')
 
     print("Вы успешно выполнили условие для выхода из порочного цикла!")
 
-except BaseException:
+except MyFirstException:
     print('Вас постигла неудача!')
