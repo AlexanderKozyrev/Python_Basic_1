a97f826c00e34dd7638770dd081db8ed3dd406f4
Alexander Kozyrev <alex18_89@mail.ru>
2022-04-08 15:46:13 +0300

    Task_5

--- Module25/05_auto/main.py
+++ Module25/05_auto/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+class Cars:
+
+    def __init__(self, x_coord, y_coord, corner):
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+        self.corner = corner
+
+    def move(self, dist):
+        self.x_coord = self.x_coord + dist * cos(self.corner)
+        self.y_coord = self.y_coord + dist * sin(self.corner)
+
+
+class Bus(Cars):
+    money = 0
+    passengers = 0
+
+    def enter(self, number):
+        self.passengers += number
+        print(f'В автобус вошло {number} пассажиров, всего в автобусе {self.passengers} пассажиров')
+
+    def exited(self, number):
+        self.passengers -= number
+        print(f'Из автобуса вышло {number} пассажиров, всего в автобусе {self.passengers} пассажиров')
+
+
