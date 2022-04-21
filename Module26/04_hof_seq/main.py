9ad9ab46437d24069da350c67043338ca92892f0
Alexander Kozyrev <alex18_89@mail.ru>
2022-04-21 11:00:50 +0300

    Task_4

--- Module26/04_hof_seq/main.py
+++ Module26/04_hof_seq/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+class Qsequence:
+
+    def __init__(self, s):
+        self.s = s[:]
+
+    def __next__(self):
+        try:
+            q = self.s[-self.s[-1]] + self.s[-self.s[-2]]
+            self.s.append(q)
+            return q
+        except IndexError:
+            raise StopIteration()
+
+    def __iter__(self):
+        return self
+
+    def current_state(self):
+        return self.s
+
