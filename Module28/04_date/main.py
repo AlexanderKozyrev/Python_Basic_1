fb6c869d1f4d490966ca9ff833043e9159f4c0d0
Alexander Kozyrev <alex18_89@mail.ru>
2022-07-06 16:52:01 +0300

    Task_4

--- Module28/04_date/main.py
+++ Module28/04_date/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+class Date:
+     def __init__(self, *args):
+          self.day, self.month, self.year = args
+
+     @classmethod
+     def parse_date_str(cls, str_date):
+          return tuple(map(int, str_date.split('-')))
+
+     @classmethod
+     def is_date_valid(cls, str_date):
+          d, m, y = cls.parse_date_str(str_date)
+          return 0 < d <= 31 and 0 < m <= 12 and y > 0
+
+     @classmethod
+     def from_string(cls, str_date):
+
