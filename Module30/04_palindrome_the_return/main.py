a87cf0b8c2769ad1b4ae525bfc193a06323610db
Alexander Kozyrev <alex18_89@mail.ru>
2022-07-14 10:44:13 +0300

    Tack_4

--- Module30/04_palindrome_the_return/main.py
+++ Module30/04_palindrome_the_return/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+from collections import Counter
+
+
+def can_be_poly(string: str) -> bool:
+    return len(list(filter(lambda x: x % 2, Counter(string).values()))) <= 2
+
+
+
+
+
+
+print(can_be_poly('abcba'))
+print(can_be_poly('abbbc'))
+print(can_be_poly('aabbc'))
