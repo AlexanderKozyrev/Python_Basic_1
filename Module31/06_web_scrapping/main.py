7e0b280c804e2bb43d265e9cf71a3eb40fcf01ef
Alexander Kozyrev <alex18_89@mail.ru>
2022-07-17 21:05:22 +0300

    Task_6

--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import requests
+import re
+
+
+url = 'http://www.columbia.edu/~fdc/sample.html'
+response = requests.get(url).text
+pattern = r'<h3.*>(.*)</h3>'
+result = re.findall(pattern, response)
+print(result)
